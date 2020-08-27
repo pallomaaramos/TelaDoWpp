2605111158018dece04b15d3ceb5341d2fb205e2
Palloma Ramos <pallomaramos@MacBook-Pro-de-Palloma.local>
2020-08-27 07:13:51 -0300

    Tela do wpp

---
+++ AguaFriaChallenge/MessageChatCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageChatCell.swift
+//  AguaFriaChallenge
+//
+//  Created by Palloma Ramos on 18/08/20.
+//  Copyright © 2020 Palloma Ramos. All rights reserved.
+//
+
+import UIKit
+
+class MessageChatCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
